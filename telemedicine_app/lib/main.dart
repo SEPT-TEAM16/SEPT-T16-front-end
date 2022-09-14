063162dedc55fdc042f9db2b1f4b9eae1ea98db9
Nelson <s3906171@rmit.student.edu.au>
2022-09-14 20:36:30 +1000

    Revert "Merge branch 'main' of https://github.com/SEPT-TEAM16/SEPT-T16-front-end"
    This reverts commit 245c978d47d27cc55740a5786385c760a6ae80ce, reversing
    changes made to 9e64a3b7c17813293058aa81e1ad0ea1bc2664ba.

--- telemedicine_app/lib/main.dart
+++ telemedicine_app/lib/main.dart
@@ -7,26 +7,10 @@ import 'package:intl/intl.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:http/http.dart' as http;
 
-<<<<<<< HEAD
-
-//REGO PAGE
-const List<Widget> Role = <Widget>[
-  Text('Patient'),
-  Text('Doctor')
-];
-
-const List<Widget> Gender = <Widget>[
-  Text('Male'),
-  Text('Female'),
-  Text('Other')
-
-];
-=======
 
 const List<String> role = <String>['Patient','Doctor'];
 
 
->>>>>>> 9e64a3b7c17813293058aa81e1ad0ea1bc2664ba
 void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -40,13 +24,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Center(child: Text(appTitle),),
         ),
-<<<<<<< HEAD
-        resizeToAvoidBottomInset: false,
-        body: SingleChildScrollView(child: RegistrationPage()),
-=======
         resizeToAvoidBottomInset: true,
         body: SingleChildScrollView(child: MyCustomForm()),
->>>>>>> 9e64a3b7c17813293058aa81e1ad0ea1bc2664ba
       ),
     );
   }
@@ -54,20 +33,15 @@ class MyApp extends StatelessWidget {
 }
 
 
-class RegistrationPage extends StatefulWidget {
-  const RegistrationPage({super.key});
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
 
   @override
-  State<RegistrationPage> createState() => _RegistrationPageState();
+  State<MyCustomForm> createState() => _MyCustomFormState();
 }
 
-<<<<<<< HEAD
-class _RegistrationPageState extends State<RegistrationPage>{
-  final List<bool> _selectedRole = <bool>[true, false];
-=======
 class _MyCustomFormState extends State<MyCustomForm>{
   TextEditingController selectedRole=new TextEditingController();
->>>>>>> 9e64a3b7c17813293058aa81e1ad0ea1bc2664ba
   TextEditingController firstName=new TextEditingController();
   TextEditingController lastName=new TextEditingController();
   TextEditingController _Date = TextEditingController();
@@ -84,20 +58,11 @@ class _MyCustomFormState extends State<MyCustomForm>{
   get child => null;
 
   Future _submit() async {
-<<<<<<< HEAD
-    final response = await http.post(Uri.parse("http://localhost:8080/api/v1/register"), body: {
-	    //"role": _selectedRole,
-      "firstName": firstName.text,
-      "lastName": lastName.text,
-      //"Gender":_selectedGender,
-      "mobile":phoneNumber.text,
-=======
     final response = await http.post(Uri.parse("http://localhost:8081/api/v1/register"),  headers: {"Content-Type": "application/json"}, body: jsonEncode({
 	    "role": selectedRole.text,
       "firstName": firstName.text,
       "lastName": lastName.text,  
       "mobileNumber":phoneNumber.text,
->>>>>>> 9e64a3b7c17813293058aa81e1ad0ea1bc2664ba
 	    "email": email.text,
       "address": "${address.text} ${city.text} ${postCode.text}",
       "password": _pass.text, 
@@ -121,29 +86,6 @@ class _MyCustomFormState extends State<MyCustomForm>{
         const Text("What is your role?"),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-<<<<<<< HEAD
-          child: ToggleButtons(isSelected: _selectedRole,
-          onPressed: (int index) {
-            setState(() {
-              for(int i = 0; i < _selectedRole.length; i++){
-                _selectedRole[i] = i == index;
-              }
-            });
-            //Set up for pushing user to next screen
-            //Navigator.pushNamed(context, '/patientprofile');
-          },
-          borderRadius: const BorderRadius.all(Radius.circular(8)),
-          selectedBorderColor: Colors.blue[700],
-          selectedColor: Colors.white,
-          fillColor: Colors.blue[200],
-          color: Colors.blue[400],
-          constraints: const BoxConstraints(
-            minHeight: 40.0,
-            minWidth: 80.0,
-          ),
-          children: Role.toList(),),
-          ),
-=======
           child: 
           DropdownButton<String>(
             value: dropdownValue,
@@ -170,7 +112,6 @@ class _MyCustomFormState extends State<MyCustomForm>{
         )
         ),
 
->>>>>>> 9e64a3b7c17813293058aa81e1ad0ea1bc2664ba
         // Personal Information
         Padding(
           padding:  EdgeInsets.symmetric(horizontal: 8, vertical: 16),
