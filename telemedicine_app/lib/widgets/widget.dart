d5ce0f4cdeaa75385cee1fd09399903401af3e35
Nelson Ngo <82756769+Nelson-Ngo@users.noreply.github.com>
2022-09-22 02:30:56 +1000

    Semi fixed button issue in doctor bookings

--- telemedicine_app/lib/widgets/widget.dart
+++ telemedicine_app/lib/widgets/widget.dart
@@ -319,12 +319,25 @@ class DoctorBookingState extends State<DoctorBooking>{
 //Builds DoctorBookingState Widget
   @override
   Widget build(BuildContext context) {
+
+    double screenwidth = MediaQuery.of(context).size.width;
+
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
+
+        const Text(
+          'Sort by Period: ',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+
         Container(
+          width: 1000,
           child: Padding(
           padding: EdgeInsets.all(10), //NEED TO FIX PADDING TO CENTER
-
           child: ToggleButtons(
             direction: vertical ? Axis.vertical : Axis.horizontal,
             onPressed: (int index) {
@@ -341,7 +354,7 @@ class DoctorBookingState extends State<DoctorBooking>{
                   color: Colors.orange[400],
                   constraints: const BoxConstraints(
                     minHeight: 40.0,
-                    minWidth: 80.0,
+                    minWidth: 122.0,
                   ),
                   isSelected: _selectedTimes,
                   children: times,
@@ -349,6 +362,7 @@ class DoctorBookingState extends State<DoctorBooking>{
           ),
         ),
 
+        //Create constructor to build new bookings per get body
         Container(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
