3f65a3c9dfb2b197b1510a10534007764cfdf03c
Nelson <s3906171@rmit.student.edu.au>
2022-09-23 05:53:00 +1000

    Deleted unused page. Started on Patient Profile

--- telemedicine_app/lib/widgets/widget.dart
+++ telemedicine_app/lib/widgets/widget.dart
@@ -1353,18 +1353,238 @@ class PatientProfileState extends State<PatientProfile>{
       children: <Widget>[
         const Text("Enter your selection"),
 
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            //controller: postCode,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'profile',
+        Container(
+          padding: EdgeInsets.all(10),
+          width: double.infinity,
+          height: 75,
+
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.blue,
+              onPrimary: Colors.white,
+              shadowColor: Colors.blueAccent,
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)
+                ),
+              minimumSize: Size(100, 40),
+            ),
+          
+          //indent please
+          child: Text("General Account Information",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 16
             )
           ),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    scrollable: true,
+                    title: Text('Change Details'),
+                    content: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            //PERSONAL INFORMATION
+                            Text('PERSONAL INFORMATION'),
+
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'First Name',
+                                icon: Icon(Icons.account_box),
+                              ),
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Last Name',
+                                icon: Icon(Icons.email),
+                              ),
+                            ),
+                            TextFormField( //change to date pickerk
+                              decoration: InputDecoration(
+                                labelText: 'Date of Birth',
+                                icon: Icon(Icons.message ),
+                              ),
+                            ),
+                            //CONTACT INFORMATION
+                            Text('CONTACT INFORMATION'),
+
+                            TextFormField( 
+                              decoration: InputDecoration(
+                                labelText: 'Email Address',
+                              ),
+                            ),
+                            TextFormField( 
+                              decoration: InputDecoration(
+                                labelText: 'Address',
+                              ),
+                            ),
+
+                            //PASSWORD
+                            Text('NEW PASSWORD'),
+
+                            TextFormField( 
+                              decoration: InputDecoration(
+                                labelText: 'New Password',
+                              ),
+                            ),
+                            TextFormField( 
+                              decoration: InputDecoration(
+                                labelText: 'Retype New Password',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                     actions: [
+                      RaisedButton(
+                          child: Text("Submit"),
+                          onPressed: () {
+                            // NEED TO DO POST HERE
+                          })
+                    ],
+                  );
+                });
+          },
+        ),
         ),
 
-        //Different Fields
+        //Conditions
+        Container(
+          padding: EdgeInsets.all(10),
+          width: double.infinity,
+          height: 75,
+
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.blue,
+              onPrimary: Colors.white,
+              shadowColor: Colors.blueAccent,
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)
+                ),
+              minimumSize: Size(100, 40),
+            ),
+
+            child: Text("Medical Conditions",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 16
+              )
+            ),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    scrollable: true,
+                    title: Text('Enter your medical conditions'),
+
+                    content: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            Text('Conditions:'),
+
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Enter into here',
+                              )
+                            )
+                          ]
+                        )
+                      )
+                    ),
+                    actions: [
+                      RaisedButton(
+                        child: Text('Submit'),
+                        onPressed: () {
+                          //NEED TO POST
+                        }
+                      )
+                    ],
+                  );
+                }
+              );
+            }
+          )
+        ),
+        
+        //Medication
+        Container(
+          padding: EdgeInsets.all(10),
+          width: double.infinity,
+          height: 75,
+
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.blue,
+              onPrimary: Colors.white,
+              shadowColor: Colors.blueAccent,
+              elevation: 3,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)
+                ),
+              minimumSize: Size(100, 40),
+            ),
+
+            child: Text("Medication",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontSize: 16
+              )
+            ),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    scrollable: true,
+
+                    title: Text('Enter your existing Medications',
+                      
+                    ),
+
+                    content: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[
+                            Text('Medication:'),
+
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Enter into here',
+                              )
+                            )
+                          ]
+                        )
+                      )
+                    ),
+                    actions: [
+                      RaisedButton(
+                        child: Text('Submit'),
+                        onPressed: () {
+                          //NEED TO POST
+                        }
+                      )
+                    ],
+                  );
+                }
+              );
+            }
+          )
+        ),
         
       ],
     );
