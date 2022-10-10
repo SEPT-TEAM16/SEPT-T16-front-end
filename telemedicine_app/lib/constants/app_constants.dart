afb85fe728af403b1bd55948742eb103e5330825
NelsonNgo0 <nelsonngo1638@gmail.com>
2022-10-10 18:44:20 +1100

    get request for bookings started

--- telemedicine_app/lib/constants/app_constants.dart
+++ telemedicine_app/lib/constants/app_constants.dart
@@ -1,8 +1,9 @@
 class ApiConstants {
-  static String baseURL = 'http://localhost:8081/api/v1';
+  static String baseURL = 'http://localhost:8082/api/v1';
   static String RegistrationEnd = '/register';
   static String AppointmentEnd = '/create-appointment';
   static String DoctorAvailabilities = '/avalibities';
+  static String AppoinmentINFO = '/get-appointments';
 
   static const phraseEndpoint = 'https://api.kanye.rest';
 
--- telemedicine_app/lib/modules/profile/models/appointment_models.dart
+++ telemedicine_app/lib/modules/profile/models/appointment_models.dart
@@ -1,24 +1,106 @@
+// To parse this JSON data, do
+//
+//     final appInfo = appInfoFromJson(jsonString);
+
+import 'package:meta/meta.dart';
 import 'dart:convert';
 
-//once we get the gets we make models.
+class AppInfo {
+    AppInfo({
+        required this.appointmentId,
+        required this.appointmentStartDate,
+        required this.appointmentEndDate,
+        required this.patient,
+        required this.doctor,
+        required this.appointmentStatus,
+    });
 
-AppointmentINFO AppfromJson(String str) => AppointmentINFO.fromJson(json.decode(str));
+    int appointmentId;
+    DateTime appointmentStartDate;
+    DateTime appointmentEndDate;
+    Doctor patient;
+    Doctor doctor;
+    String appointmentStatus;
 
-class AppointmentINFO {
-  
-  AppointmentINFO({
-    required this.appointmentStartDate, //ask lucas how to ignore null
+    factory AppInfo.fromRawJson(String str) => AppInfo.fromJson(json.decode(str));
 
-  });
+    String toRawJson() => json.encode(toJson());
 
-  String appointmentStartDate;
+    factory AppInfo.fromJson(Map<String, dynamic> json) => AppInfo(
+        appointmentId: json["appointmentId"],
+        appointmentStartDate: DateTime.parse(json["appointmentStartDate"]),
+        appointmentEndDate: DateTime.parse(json["appointmentEndDate"]),
+        patient: Doctor.fromJson(json["patient"]),
+        doctor: Doctor.fromJson(json["doctor"]),
+        appointmentStatus: json["appointmentStatus"],
+    );
 
-  factory AppointmentINFO.fromJson(Map<String, dynamic> json) => AppointmentINFO(
-    appointmentStartDate: json["appointmentStartDate"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "appointmentStartDate": appointmentStartDate,
-  };
+    Map<String, dynamic> toJson() => {
+        "appointmentId": appointmentId,
+        "appointmentStartDate": appointmentStartDate.toIso8601String(),
+        "appointmentEndDate": appointmentEndDate.toIso8601String(),
+        "patient": patient.toJson(),
+        "doctor": doctor.toJson(),
+        "appointmentStatus": appointmentStatus,
+    };
 }
 
+class Doctor {
+    Doctor({
+        required this.userId,
+        required this.email,
+        required this.firstName,
+        required this.lastName,
+        required this.password,
+        required this.doB,
+        required this.accountActive,
+        required this.address,
+        required this.mobileNumber,
+        required this.role,
+        required this.doctorDoB,
+    });
+
+    int userId;
+    String email;
+    String firstName;
+    String lastName;
+    String password;
+    String doB;
+    bool accountActive;
+    String address;
+    String mobileNumber;
+    String role;
+    String doctorDoB;
+
+    factory Doctor.fromRawJson(String str) => Doctor.fromJson(json.decode(str));
+
+    String toRawJson() => json.encode(toJson());
+
+    factory Doctor.fromJson(Map<String, dynamic> json) => Doctor(
+        userId: json["userId"],
+        email: json["email"],
+        firstName: json["firstName"],
+        lastName: json["lastName"],
+        password: json["password"],
+        doB: json["DoB"],
+        accountActive: json["accountActive"],
+        address: json["address"],
+        mobileNumber: json["mobileNumber"],
+        role: json["role"],
+        doctorDoB: json["doB"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "email": email,
+        "firstName": firstName,
+        "lastName": lastName,
+        "password": password,
+        "DoB": doB,
+        "accountActive": accountActive,
+        "address": address,
+        "mobileNumber": mobileNumber,
+        "role": role,
+        "doB": doctorDoB,
+    };
+}
